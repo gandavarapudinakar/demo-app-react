d4381854de4374708992a6e0ee55c18e931db7fc
raju <raju@zessta.com>
2019-01-07 18:47:28 +0530

    initial commit

---
+++ src/components/FormRow.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+const formrow=(props)=>{
+    return (
+        <div className="row">
+            <div className="cell"><strong>{props.label}</strong></div>
+            <div className="cell"><input type={props.type} value={props.value} onChange={props.onChangeHandler}/></div>
+        </div>
+    );
+}
+export default formrow;
